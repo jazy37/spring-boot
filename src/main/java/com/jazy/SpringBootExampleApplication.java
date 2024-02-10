56468109424fdd0bae62b5871424c230649092eb
jazy37 <jakubkulesza02@gmail.com>
2024-02-10 17:51:54 +0100

    Initializing the project

---
+++ src/main/java/com/jazy/SpringBootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.jazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jazy/SpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jazy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
