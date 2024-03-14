8dd13c4f726a645b922fb4cb077b72d76fba673a
jazy37 <jakubkulesza02@gmail.com>
2024-03-14 23:35:34 +0100

    CORS, vite FrontEnd, SideBar, Axios

---
+++ frontend/react/src/App.jsx
@@ -0,0 +1,64 @@
+import SidebarWithHeader from "./components/shared/SideBar.jsx";
+import {Spinner, Text, Card, Wrap, WrapItem} from "@chakra-ui/react";
+import {useEffect, useState} from "react";
+import {getCustomers} from "./services/client.js";
+import SocialProfileWithImage from "./components/Card.jsx";
+import CardWithImage from "./components/Card.jsx";
+
+
+function App() {
+
+    const [customers, setCustomers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        getCustomers()
+            .then(res => {
+               setCustomers(res.data)
+            }).catch(err => {
+                console.log(err)
+        }).finally(() => setLoading(false))
+    }, []);
+
+    if(loading) {
+        return (
+            <SidebarWithHeader>
+                <Spinner
+                    thickness='4px'
+                    speed='0.65s'
+                    emptyColor='gray.200'
+                    color='blue.500'
+                    size='xl'
+                />
+            </SidebarWithHeader>
+            )
+    }
+
+    if(customers.length <= 0) {
+        return (
+            <div>
+                <SidebarWithHeader>
+                    <Text>No customers available</Text>
+                </SidebarWithHeader>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <SidebarWithHeader>
+                <Wrap jusify={"center"} spacing={"30px"}>
+                {customers.map((customer, index) => (
+                     (<WrapItem key={index}>
+                        <CardWithImage key={index} {...customer} />
+                    </WrapItem>)
+                ))}
+                </Wrap>
+            </SidebarWithHeader>
+        </div>
+    )
+}
+
+
+export default App
